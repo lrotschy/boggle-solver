d9d2d78a3c88a0f955a524573cacfab47776b64c
Liela Rotschy <lielarotschy@gmail.com>
2020-07-27 14:31:41 -0600

    Add project files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import solver from './solver.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class BoggleSolver extends Component {
+
+  constructor(...props) {
+    super(...props);
+    this.state = this.defaultState;
+    this.updateRow = this.updateRow.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleError = this.handleError.bind(this);
+  }
+
+  defaultState = {
+    rowOne: '',
+    rowTwo: '',
+    rowThree: '',
+    rowFour: ''
+  }
+
+  updateRow(event) {
+    let row = event.target.name;
+    let value = event.target.value;
+
+    this.setState({
+      [row]: value
+    })
+
+  }
+
+  handleError() {
+    document.querySelector("#words").innerHTML = "Input should only contain letters a - z"
+  }
+
+  inputError(boardStrings) {
+    return (/[^a-zA-Z]/).test(boardStrings.join(""));
+  }
+
+  createBoard(boardStrings) {
+    return boardStrings.map(string => string.toLowerCase().split(""));
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let boardStrings = [
+      this.state.rowOne,
+      this.state.rowTwo,
+      this.state.rowThree,
+      this.state.rowFour
+    ]
+
+    if (this.inputError(boardStrings)) {
+      this.handleError();
+    } else {
+      let board = this.createBoard(boardStrings)
+      let words = solver.findAllWords(board);
+      let result = words.length === 0 ? "No words can be formed from this input" : words.join(", ");
+      document.querySelector("#words").innerHTML = result;
+    }
+  }
+
+  handleReset(event) {
+    event.preventDefault();
+
+    this.setState(this.defaultState);
+    document.querySelector("#words").innerHTML = ""
+  }
+
+  render() {
+    return (
+      <div id="boggle-solver">
+        <h1>Boggle Solver</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="rowOne">Row One:</label>
+          <input
+            name="rowOne"
+            size="4"
+            maxLength="4"
+            minLength="4"
+            value={this.state.rowOne}
+            onChange={this.updateRow}>
+          </input>
+
+          <label htmlFor="rowTwo">Row Two:</label>
+          <input
+            name="rowTwo"
+            size="4"
+            maxLength="4"
+            minLength="4"
+            value={this.state.rowTwo}
+            onChange={this.updateRow}>
+          </input>
+
+          <label htmlFor="rowThree">Row Three:</label>
+          <input
+            name="rowThree"
+            size="4"
+            maxLength="4"
+            minLength="4"
+            value={this.state.rowThree}
+            onChange={this.updateRow}>
+          </input>
+
+          <label htmlFor="rowFour">Row Four:</label>
+          <input
+            name="rowFour"
+            size="4"
+            maxLength="4"
+            minLength="4"
+            value={this.state.rowFour}
+            onChange={this.updateRow}>
+          </input>
+
+          <button type="submit">Submit</button>
+          <button onClick={this.handleReset}>Reset Form</button>
+        </form>
+
+        <div name="result">
+          <h2>Words that can be created from this board:</h2>
+          <p id="words"></p>
+        </div>
+      </div>
+    )
+
+  }
 }
 
-export default App;
+export default BoggleSolver;
---
+++ src/solver.js
@@ -0,0 +1,88 @@
+import scrabble from 'scrabble';
+
+const solver = {
+  boardSize: 4,
+  findAllWords: function (board) {
+    const dictionary = scrabble(board.flat().join(''));
+    let result = [];
+    let i;
+    let j;
+
+    for (i = 0; i < board.length; i += 1) {
+      for (j = 0; j < board[0].length; j += 1) {
+        let words = this.findWords(board, dictionary, [i, j]);
+        result = result.concat(words)
+      }
+    }
+
+    return result.filter((word, idx) => {
+      return idx === result.lastIndexOf(word);
+    });
+  },
+  findWords: function (board, dictionary, start) {
+    const string = board[start[0]][start[1]];
+    const queue = [
+      [string, [start]]
+    ];
+    let result = [];
+
+    while (queue.length > 0) {
+      const currentValue = queue.pop();
+      const currentString = currentValue[0]
+      const visitedCoordinates = currentValue[1];
+      const lastCoordinates = visitedCoordinates[visitedCoordinates.length - 1];
+      const neighbors = this.listNeighbors(lastCoordinates);
+
+      neighbors.forEach(newCoordinates => {
+        const v = newCoordinates[0]
+        const h = newCoordinates[1]
+
+        if (this.legitimateCoordinates(v, h) && !visitedCoordinates.includes(newCoordinates)) {
+          let newString = currentString + board[v][h];
+
+          if (newString.length > 2 && dictionary.includes(newString) && !result.includes(newString)) {
+            result.push(newString);
+          }
+
+          if (this.prefix(newString, dictionary) && newString.length < 16) {
+            let visited = visitedCoordinates.concat([newCoordinates]);
+            queue.unshift([newString, visited]);
+          }
+
+        }
+      })
+    }
+    return result;
+  },
+  legitimateCoordinates: function (v, h) {
+    return (v >= 0 && v < this.boardSize && h >= 0 && h < this.boardSize);
+  },
+  listNeighbors: function (coordinates) {
+    let v = coordinates[0];
+    let h = coordinates[1];
+
+    return [
+      [v, h + 1],
+      [v, h - 1],
+      [v - 1, h],
+      [v + 1, h],
+      [v + 1, h + 1],
+      [v + 1, h - 1],
+      [v - 1, h + 1],
+      [v - 1, h - 1]
+    ]
+  },
+  prefix: function (string, dictionary) {
+    let i;
+    let reg = new RegExp('^' + string + '\\B');
+
+    for (i = 0; i < dictionary.length; i += 1) {
+      if (reg.test(dictionary[i])) {
+        return true;
+      }
+    }
+    return false;
+  }
+}
+
+export default solver;
